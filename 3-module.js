74e35675487985c5fd603579a6be0e5b8fd37f2d
Rahul Mallick <rahulmallick1115@gmail.com>
2022-08-19 21:20:18 +0530

    first commit

---
+++ 3-module.js
@@ -0,0 +1,11 @@
+// Modules
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+// console.log(names);
+
+const data = require("./6-alternative-flavor");
+console.log(data);
+
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
